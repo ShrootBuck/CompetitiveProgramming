1efd817f85a996e90b1e26a837b84bdd3161b22e
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-07-23 14:19:09 -0700

    Add solution for USACO Bronze 2018 bucket list problem

---
+++ 2018/Bronze/bucket-list.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(std::string name = "") {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+struct cow {
+  int s;
+  int t;
+  int b;
+};
+
+int main() {
+  setIO("blist");
+
+  int N;
+  cin >> N;
+
+  int total_time = 0;
+
+  vector<cow> unmilked_cows(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> unmilked_cows[i].s;
+    cin >> unmilked_cows[i].t;
+    cin >> unmilked_cows[i].b;
+
+    total_time = max(total_time, unmilked_cows[i].t);
+  };
+
+  int buckets_in_use = 0;
+  int max_buckets_in_use = 0;
+
+  for (int i = 1; i <= total_time; i++) {
+    for (cow cow : unmilked_cows) {
+      if (i > cow.s) {
+        if (i < cow.t) {
+          // Is milking
+        } else if (cow.t == i) {
+          buckets_in_use -= cow.b;
+        }
+      } else if (cow.s == i) {
+        int buckets_left = cow.b;
+        while (buckets_left > cow.b) {
+          buckets_in_use++;
+          buckets_left--;
+        }
+
+        buckets_in_use += buckets_left;
+      }
+    }
+
+    max_buckets_in_use = max(max_buckets_in_use, buckets_in_use);
+  }
+
+  cout << max_buckets_in_use;
+}
--- snippets.cpp
+++ snippets.cpp
@@ -1,8 +1,8 @@
 // Copy-paste central for USACO and competitive programming
 
-void setIO(std::string name = "") {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   if (!name.empty()) {
     freopen((name + ".in").c_str(), "r", stdin);
     freopen((name + ".out").c_str(), "w", stdout);
