58e949d7192eba282377d1e829d55f3bac4c8579
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-08-09 23:37:32 -0700

    Add milk pails problem solution

---
+++ 2016/Bronze/milk-pails.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("pails");
+
+  int X, Y, M;
+  cin >> X >> Y >> M;
+
+  int total = 0;
+
+  for (int i = 0; i * X <= M; i++) {
+    for (int j = 0; i * X + j * Y <= M; j++) {
+      int newmax = X * i + Y * j;
+      total = max(total, newmax);
+    }
+  }
+
+  cout << total;
+}
