7871bc9732bed18768a71516e433dd7838b1303c
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-08-09 23:37:41 -0700

    Add two solutions for diamond collector problem
    Implements both brute force O(n²) and sliding window O(n) approaches for
    finding maximum diamonds collectible within size constraint K

---
+++ 2016/Bronze/diamond-collector-2.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("diamond");
+
+  size_t N;
+  size_t K;
+  cin >> N >> K;
+
+  vector<int> rocks(N);
+
+  for (size_t i = 0; i < N; i++) {
+    cin >> rocks[i];
+  }
+
+  sort(rocks.begin(), rocks.end());
+
+  size_t count = 1;
+
+  // Sliding window
+  // What an elegant solution I wish I thought of this earlier
+  size_t left = 0;
+  size_t right = 0;
+
+  while (right < N) {
+    while (right < N && rocks[right] - rocks[left] <= K) {
+      right++;
+    }
+
+    count = max(count, right - left);
+
+    left++;
+  }
+
+  cout << count;
+}
---
+++ 2016/Bronze/diamond-collector.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("diamond");
+
+  int N;
+  int K;
+  cin >> N >> K;
+
+  vector<int> rocks(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> rocks[i];
+  }
+
+  sort(rocks.begin(), rocks.end());
+
+  int count = 1;
+
+  for (int i = 0; i < N; i++) {
+    int local_count = 1;
+    for (int j = i + 1; j < N && rocks[j] - rocks[i] <= K; j++) {
+      local_count++;
+    }
+
+    count = max(count, local_count);
+  }
+  cout << count;
+}
