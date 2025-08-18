31105edb07102992040a2b6c5a787a5bbb2682b1
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-08-17 17:54:45 -0700

    Add Counting Liars problem solution

---
+++ 2022/Bronze/counting-liars.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  size_t N;
+  cin >> N;
+
+  size_t liars = N;
+
+  vector<char> sightings(N);
+  vector<size_t> p(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> sightings[i];
+    cin >> p[i];
+  }
+
+  for (int i = 0; i < N; i++) {
+    size_t local_liars = 0;
+    for (int j = 0; j < N; j++) {
+      if (sightings[j] == 'G') {
+        if (p[j] > p[i]) {
+          local_liars++;
+        }
+      } else {
+        if (p[j] < p[i]) {
+          local_liars++;
+        }
+      }
+    }
+
+    liars = min(liars, local_liars);
+  }
+
+  cout << liars;
+}
