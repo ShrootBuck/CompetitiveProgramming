6eff0c161c0240586c6b2835ee5b51ec79524aa5
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-08-27 20:04:28 -0700

    Add solution and image for Bronze problem Why Did the Cow Cross the Road II

---
+++ 2017/Bronze/why-did-the-cow-cross-the-road-ii.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <utility>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("circlecross");
+
+  string input;
+  cin >> input;
+  map<char, pair<int, int>> cow_indices;
+  unordered_map<char, int> first_position;
+
+  // Load in the cow indices
+  for (int i = 0; i < input.length(); i++) {
+    char cow = input[i];
+
+    if (first_position.count(cow)) {
+      cow_indices[cow] = {first_position[cow], i};
+    } else {
+      first_position[cow] = i;
+    }
+  }
+
+  unordered_map<int, unordered_map<int, bool>> crossings;
+  int total = 0;
+
+  for (int i = 0; i < 26; i++) {
+    char current_cow = 'A' + i;
+    int overlap = 0;
+    unordered_map<char, bool> unfolded{};
+
+    for (int j = cow_indices[current_cow].first + 1;
+         j < cow_indices[current_cow].second; j++) {
+      if (unfolded[input[j]]) {
+        overlap--;
+        // We could clean it from unfolded here but not really necessary
+      } else {
+        unfolded[input[j]] = true;
+        overlap++;
+      }
+    }
+
+    total += overlap;
+  }
+
+  cout << total / 2;
+}
