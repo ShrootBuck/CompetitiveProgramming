3a86f72d9cf23d1b37cfcd97a2cd6cc03b963a1a
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-09-22 19:54:48 -0700

    Add CSES apple division problem solution

---
+++ CSES/apple-division.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+vector<int> search_space{};
+int N;
+vector<size_t> p;
+
+size_t min_diff = ULONG_MAX;
+size_t total = 0;
+
+// Permute each set
+void search(int k) {
+  if (k == N) {
+    size_t local_sum = 0;
+    for (int weight : search_space) {
+      local_sum += p[weight];
+    }
+
+    min_diff = min(min_diff, 2 * local_sum - total);
+  } else {
+    search(k + 1);
+    search_space.push_back(k);
+    search(k + 1);
+    search_space.pop_back();
+  }
+}
+
+int main() {
+  cin >> N;
+
+  for (int i = 0; i < N; ++i) {
+    int weight;
+    cin >> weight;
+    total += weight;
+    p.push_back(weight);
+  }
+
+  // Search the entire space
+  search(0);
+
+  cout << min_diff;
+}
