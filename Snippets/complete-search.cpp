599c3297e0dac20d17ef78708f9bc97f15f95036
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-09-22 20:08:08 -0700

    Add complete search subset generation algorithm

---
+++ Snippets/complete-search.cpp
@@ -0,0 +1,25 @@
+int n;
+vector<int> current_subset;
+
+// Recursive function to explore all subsets
+void generate(int k) {
+  if (k == n) {
+    // Process the subset
+  }
+
+  // Choice 1: "leave it"
+  // We don't add item k to our subset and move on to the next item
+  generate(k + 1);
+
+  // Choice 2: "take it"
+  // We add item k to our subset
+  current_subset.push_back(k);
+  // ...and then move on to the next item
+  generate(k + 1);
+  // ...and then we MUST backtrack by removing it, to clean up the state
+  // for other branches of the recursion.
+  current_subset.pop_back();
+}
+
+// Kick off the recursion from the first item (index 0)
+// generate(0);
