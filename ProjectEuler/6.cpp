934684a7749b8f061dc71fbad64652afd81518b2
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-09-27 20:36:56 -0700

    Add Project Euler problem 6 solution

---
+++ ProjectEuler/6.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+  int n = 100;
+
+  int sum_of_squares = 0;
+  int square_of_sum = 0;
+
+  for (int i = 1; i <= n; i++) {
+    sum_of_squares += i * i;
+    square_of_sum += i;
+  }
+
+  square_of_sum *= square_of_sum;
+
+  cout << abs(sum_of_squares - square_of_sum);
+}
