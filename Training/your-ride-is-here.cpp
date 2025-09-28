cbb7f43384118d9638b49db89cb92dc8ccf966f2
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-09-27 20:37:03 -0700

    Add solution for "Your Ride Is Here" problem

---
+++ Training/your-ride-is-here.cpp
@@ -0,0 +1,46 @@
+/*
+ID: zayd1
+LANG: C++
+TASK: ride
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("ride");
+
+  string a, b;
+  cin >> a >> b;
+
+  int numa = 1;
+  int numb = 1;
+
+  for (auto c : a) {
+    numa *= c - 'A' + 1;
+  }
+
+  for (auto c : b) {
+    numb *= c - 'A' + 1;
+  }
+
+  numa %= 47;
+  numb %= 47;
+
+  if (numa == numb) {
+    cout << "GO";
+  } else {
+    cout << "STAY";
+  }
+
+  cout << "\n";
+  return 0;
+}
