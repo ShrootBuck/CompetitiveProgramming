773f09e3e8d3c2b6cf3b95be449385db29a29f7e
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-01 17:14:49 -0700

    Add CSES distinct numbers solution

---
+++ CSES/distinct-numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  set<int> nums;
+  for (int i = 0; i < N; i++) {
+    int x;
+    cin >> x;
+    nums.insert(x);
+  }
+
+  cout << nums.size();
+}
