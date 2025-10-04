f8af934405c1a5c10d174c1b1af347e4f9283f27
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-04 12:00:35 -0700

    Add Project Euler 10 solution: sum primes under 2M

---
+++ Project Euler/10.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  vector<bool> nums(2000000 + 1, true);
+  nums[0] = false;
+  nums[1] = false;
+
+  // Sieve of whatever his name was
+  for (int i = 2; i <= sqrt(2000000); i++) {
+    if (nums[i]) {
+      for (int j = 2 * i; j <= 2000000; j += i) {
+        nums[j] = false;
+      }
+    }
+  }
+
+  long long sum = 0;
+
+  for (int i = 1; i < nums.size(); i++) {
+    if (nums[i]) {
+      sum += i;
+    }
+  }
+
+  cout << sum;
+}
