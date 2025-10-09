012d86114bc1d6edc88ad86fcdf2f4f84c5d07b1
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-09 10:52:00 -0700

    Replace individual includes with bits/stdc++.h

--- 2017/Bronze/lost-cow.cpp
+++ 2017/Bronze/lost-cow.cpp
@@ -1,6 +1,4 @@
-#include <cmath>
-#include <fstream>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
--- CSES/permutations.cpp
+++ CSES/permutations.cpp
@@ -1,4 +1,3 @@
-// Permutations
 #include <bits/stdc++.h>
 using namespace std;
 
