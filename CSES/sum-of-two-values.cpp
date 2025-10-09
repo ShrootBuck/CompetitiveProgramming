d2bf067131c65896c4ca68c9ebef29aaf1cd2799
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-08 17:18:23 -0700

    Add CSES sum-of-two-values solution

---
+++ CSES/sum-of-two-values.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  size_t n, x;
+  cin >> n >> x;
+
+  map<size_t, size_t> cache; // hardly a cache but ok
+
+  for (int i = 0; i < n; i++) {
+    size_t newnum;
+    cin >> newnum;
+
+    if (cache.contains(x - newnum)) {
+      cout << cache[x - newnum] + 1 << " " << i + 1;
+      return 0;
+    }
+
+    cache[newnum] = i;
+  }
+
+  cout << "IMPOSSIBLE";
+}
