b79dfe89a7634cfdb7cd95959308ebfa97923641
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-10 16:32:48 -0700

    Add solution for Codeforces problem 2153

---
+++ Codeforces/2153/circle-of-apple-trees.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("");
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    set<int> beauty;
+
+    for (int i = 0; i < n; i++) {
+      int apple_beauty;
+      cin >> apple_beauty;
+
+      beauty.insert(apple_beauty);
+    }
+
+    cout << beauty.size() << "\n";
+  }
+}
