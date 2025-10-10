06469a3ce4ca5ca8f9d8b90dd0a9d7a6216b98d3
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-10 16:32:54 -0700

    Add Codeforces 2148 lasers solution

---
+++ Codeforces/2148/lasers.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("");
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int crosses = 0;
+
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+
+    for (int i = 0; i < n; i++) {
+      int laser_y;
+      cin >> laser_y;
+
+      crosses += (laser_y <= y);
+    }
+
+    for (int i = 0; i < m; i++) {
+      int laser_x;
+      cin >> laser_x;
+
+      crosses += (laser_x <= x);
+    }
+
+    cout << crosses << "\n";
+  }
+}
