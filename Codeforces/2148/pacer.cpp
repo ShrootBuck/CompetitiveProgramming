b18c8bb2f0806aa769ef1c9f49ede394d5250a08
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-10 16:33:06 -0700

    Add Codeforces problem 2148 pacer solution

---
+++ Codeforces/2148/pacer.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO();
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<int, int>> requirements{{0, 0}};
+    int points = 0;
+
+    for (int i = 0; i < n; i++) {
+      int a, b;
+      cin >> a >> b;
+      requirements.push_back({a, b});
+    }
+
+    for (int i = 0; i < n; i++) {
+      int time_diff = requirements[i + 1].first - requirements[i].first;
+
+      int start_side = requirements[i].second;
+      int end_side = requirements[i + 1].second;
+
+      if (time_diff % 2 == 0) {
+        if (end_side == start_side) {
+          points += time_diff;
+        } else {
+          points += time_diff - 1;
+        }
+      } else {
+        if (end_side != start_side) {
+          points += time_diff;
+        } else {
+          points += time_diff - 1;
+        }
+      }
+    }
+
+    // Add remaining laps post-requirements
+    points += m - requirements[n].first;
+
+    cout << points << "\n";
+  }
+}
