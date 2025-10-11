449d0ca2fad24491b21c30fd04732d38fc62832b
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-10 20:39:13 -0700

    Add Codeforces 2148 Destruction of the Dandelions solution

---
+++ Codeforces/2148/destruction-of-the-dandelion-fields.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  ll count = 0;
+  vector<int> odds;
+
+  for (int i = 0; i < n; i++) {
+    int d;
+    cin >> d;
+
+    if (d % 2 == 0) {
+      count += d;
+    } else {
+      odds.push_back(d);
+    }
+  }
+
+  if (odds.size() == 0) {
+    cout << 0;
+    return;
+  }
+
+  sort(odds.begin(), odds.end());
+
+  // Add the largest odds that we don't sacrifice
+  for (int i = odds.size() / 2; i < odds.size(); i++) {
+    count += odds[i];
+  }
+
+  cout << count;
+}
+
+int main() {
+  setIO("");
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    solve();
+    cout << "\n";
+  }
+}
