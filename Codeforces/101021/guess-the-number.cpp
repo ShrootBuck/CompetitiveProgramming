ff7fae82f3710a379e81c9c7d85dcaadafb39a55
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-12 10:54:39 -0700

    Add Codeforces 101021 guess-the-number solution (my first interactive I/O problem!)

---
+++ Codeforces/101021/guess-the-number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+// Just to flush out buffer
+void print(const string &s) { cout << s << endl; }
+
+int main() {
+  int l, r;
+  l = 1;
+  r = 1000000;
+
+  while (r != l) {
+    int guess = (r + l + 1) / 2;
+
+    print(to_string(guess));
+
+    string result;
+    cin >> result;
+
+    if (result == "<") {
+      r = guess - 1;
+    } else if (result == ">=") {
+      l = guess;
+    }
+  }
+
+  print("! " + to_string(l));
+}
