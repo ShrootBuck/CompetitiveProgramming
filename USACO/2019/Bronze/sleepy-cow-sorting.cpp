dde54cbc4cb0f5a8e6971c9b9249360a8ee4d0e0
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-18 14:36:14 -0700

    USACO: Add 2019 Bronze Sleepy Cow Sorting solution

---
+++ USACO/2019/Bronze/sleepy-cow-sorting.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  if (!name.empty()) {
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("sleepy");
+
+  int N;
+  cin >> N;
+
+  vector<int> arr(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+
+  int sorted_caboose = 1;
+
+  for (int i = N - 2; i >= 0; i--) {
+    if (arr[i] < arr[i + 1]) {
+      sorted_caboose++;
+    } else {
+      break;
+    }
+  }
+
+  cout << N - sorted_caboose;
+}
