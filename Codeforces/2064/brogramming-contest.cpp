f2090288b94e95b42e24b7c4fd9802c9ae2d38ff
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-18 22:08:06 -0700

    Add solution for Codeforces 2064 Brogramming Contest

---
+++ Codeforces/2064/brogramming-contest.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    bool all_zero = true;
+    bool all_one = true;
+
+    for (char &c : s) {
+        if (c == '0') {
+            all_one = false;
+        } else {
+            all_zero = false;
+        }
+    }
+
+    if (all_zero) {
+        cout << 0;
+        return;
+    }
+
+    if (all_one) {
+        cout << 1;
+        return;
+    }
+
+    char last = '0';
+
+    int groups = 0;
+
+    for (auto &c : s) {
+        if (c != last) {
+            groups++;
+        }
+
+        last = c;
+    }
+
+    cout << groups;
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+}
