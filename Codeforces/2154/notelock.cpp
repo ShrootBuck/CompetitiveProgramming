6f01950a373551cc647a155e87f32cc16a98af7e
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-20 08:45:57 -0700

    Add solution for Codeforces problem 2154 notelock

---
+++ Codeforces/2154/notelock.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    int protection_remaining = -1;
+    int need_protection = 0;
+
+    for (int i = 0; i < n; i++) {
+        auto &c = s[i];
+
+        if (c == '1') {
+            if (protection_remaining <= 0) {
+                need_protection++;
+            }
+
+            protection_remaining = k;
+        }
+
+        protection_remaining--;
+    }
+
+    cout << need_protection;
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+}
