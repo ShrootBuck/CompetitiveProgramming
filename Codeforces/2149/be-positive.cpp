0b65723fd356a20827e853091f1318dcdbfcf473
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-23 08:50:14 -0700

    Add Codeforces problem 2149 Be Positive solution

---
+++ Codeforces/2149/be-positive.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int solve() {
+    int n;
+    cin >> n;
+
+    vector<int> negatives;
+
+    int swaps = 0;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if (num == 0) {
+            swaps++;
+        } else if (num < 0) {
+            negatives.push_back(num);
+        }
+    }
+
+    sort(negatives.begin(), negatives.end());
+
+    if (negatives.size() % 2 == 0) {
+        return swaps;
+    } else {
+        return swaps + abs(negatives[0]) + 1;
+    }
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
