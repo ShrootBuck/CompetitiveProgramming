2fcf3f2e9e57c3b03d30427a090b1d79db82f08a
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-25 19:14:16 -0700

    Add USACO 2023 Bronze Watching Mooloo solution

---
+++ USACO/2023/Bronze/watching-mooloo.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    ll N, K;
+    cin >> N >> K;
+
+    vector<ll> days(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> days[i];
+    }
+
+    ll cost = 1 + K;
+
+    for (int i = 1; i < N; i++) {
+        if (days[i] - days[i - 1] <= K + 1) {
+            cost += days[i] - days[i - 1];
+        } else {
+            cost += K + 1;
+        }
+    }
+
+    cout << cost;
+}
