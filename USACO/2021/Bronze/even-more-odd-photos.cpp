f659ce42007e52a4a57100a22388a7d7281aa51e
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-27 16:23:22 -0700

    Add USACO 2021 Bronze even more odd photos solution

---
+++ USACO/2021/Bronze/even-more-odd-photos.cpp
@@ -0,0 +1,67 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    ll N;
+    cin >> N;
+
+    ll even = 0;
+    ll odd = 0;
+
+    for (int i = 0; i < N; i++) {
+        ll num;
+        cin >> num;
+
+        if (num % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    ll count = 0;
+    ll goal = 0;
+
+    while (even + odd >= 1) {
+        if (goal == 0) {
+            if (even >= 1) {
+                even--;
+            } else if (odd >= 2) {
+                odd -= 2;
+            } else {
+                break;
+            }
+        } else {
+            if (odd >= 1) {
+                odd--;
+            } else {
+                break;
+            }
+        }
+
+        goal ^= 1; // Very useful XOR application
+        count++;
+    }
+
+    // Must merge two groups
+    if (goal == 0 && odd == 1) {
+        count--;
+    }
+
+    cout << count;
+}
