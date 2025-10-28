60d2a1235d0297f655b8eeb1881e9a87f749749b
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-28 09:14:04 -0700

    Add Codeforces studying algorithms greedy solution

---
+++ Codeforces/102951/studying-algorithms.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int N, X;
+    cin >> N >> X;
+
+    vector<int> a(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    int time_spent = 0;
+    int algos = 0;
+
+    for (int &b : a) {
+        if (time_spent + b <= X) {
+            time_spent += b;
+            algos++;
+        } else {
+            break;
+        }
+    }
+
+    cout << algos;
+}
