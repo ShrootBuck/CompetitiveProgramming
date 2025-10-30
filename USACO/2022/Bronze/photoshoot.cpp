3662dc5bff46f2ed0072ff9e1069fff7df1113b1
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-10-30 08:09:39 -0700

    Solve USACO 2022 Bronze Photoshoot

---
+++ USACO/2022/Bronze/photoshoot.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int N;
+    cin >> N;
+
+    string s;
+    cin >> s;
+
+    int reversals = 0;
+
+    for (int c = N - 2; c >= 0; c -= 2) {
+        string sub = s.substr(c, 2);
+
+        if (sub[0] == sub[1]) {
+            // Reversal doesn't matter for maximization
+            continue;
+        }
+
+        if ((sub == "GH" && reversals % 2 == 0) ||
+            (sub == "HG" && reversals % 2 == 1)) {
+            reversals++;
+        }
+    }
+
+    cout << reversals;
+}
