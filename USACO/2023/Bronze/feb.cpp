9f21625d75b538f3ac87aa9893850637b72941a5
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-03 16:48:43 -0700

    Add USACO 2023 Bronze FEB solution

---
+++ USACO/2023/Bronze/feb.cpp
@@ -0,0 +1,108 @@
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int N;
+    cin >> N;
+
+    string s;
+    cin >> s;
+
+    int min_value = 0, max_value = 0;
+    int i = 0;
+
+    while (i < N) {
+
+        // Advance b/c F
+        if (s[i] == 'F') {
+            i++;
+            continue;
+        }
+
+        int next_i = i + 1;
+        while (next_i < N && s[next_i] == 'F') {
+            next_i++;
+        }
+
+        if (next_i == N) { // Overshot
+            break;
+        }
+
+        int f_count = next_i - i - 1;
+
+        if (s[next_i] == s[i]) {
+            // Case: BFFF...FFFB / EFFF...FFFE
+
+            if ((f_count + 2) % 2 == 0) {
+                min_value++;
+            }
+
+            max_value += f_count + 1;
+        } else {
+            // Case: BFFF...FFFE / EFFF...FFFB
+
+            if ((f_count + 2) % 2 != 0) {
+                min_value++;
+            }
+
+            max_value += f_count;
+        }
+
+        i = next_i;
+    }
+
+    // Special Case: Prefix/Suffix F
+    int beginning_f = 0;
+    int ending_f = 0;
+
+    while (beginning_f < N && s[beginning_f] == 'F') {
+        beginning_f++;
+    }
+    while (ending_f < N && s[N - 1 - ending_f] == 'F') {
+        ending_f++;
+    }
+
+    if (beginning_f == N) {
+        // Special Case: Everything is an F
+        min_value = 0;
+        max_value = N - 1;
+    } else {
+        max_value += beginning_f + ending_f;
+    }
+
+    vector<int> candidates;
+
+    if (beginning_f == 0 && ending_f == 0) {
+        // Special Case: We can only change in increments of two
+        for (int i = min_value; i <= max_value; i += 2) {
+            candidates.push_back(i);
+        }
+    } else {
+        for (int i = min_value; i <= max_value; i++) {
+            candidates.push_back(i);
+        }
+    }
+
+    cout << candidates.size() << "\n";
+
+    for (int i : candidates) {
+        cout << i << "\n";
+    }
+}
