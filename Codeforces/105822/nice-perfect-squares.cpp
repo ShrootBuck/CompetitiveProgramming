1865ef17e874cf40dd1aa13aa05cd62640cf8f98
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-06 07:33:32 -0700

    Add solution for Codeforces 105822

---
+++ Codeforces/105822/nice-perfect-squares.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve() {
+    int N;
+    cin >> N;
+
+    if (N % 2 == 0) {
+        cout << "2025";
+
+        for (int i = 0; i < N - 4; i++) {
+            cout << "0";
+        }
+    } else {
+        cout << "42025";
+
+        for (int i = 0; i < N - 5; i++) {
+            cout << "0";
+        }
+    }
+}
+
+int main() {
+    setIO();
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        solve();
+        cout << "\n";
+    }
+}
