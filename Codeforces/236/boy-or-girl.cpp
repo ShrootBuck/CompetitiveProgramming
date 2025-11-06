edb02069abc8ec469471d3d482dceb86d84f1c18
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-06 10:17:35 -0700

    Add solution for Codeforces 236 boy-or-girl problem

---
+++ Codeforces/236/boy-or-girl.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    string s;
+    cin >> s;
+
+    set<char> unique;
+
+    for (auto &c : s) {
+        unique.insert(c);
+    }
+
+    if (unique.size() % 2 == 0) {
+        cout << "CHAT WITH HER!";
+    } else {
+        cout << "IGNORE HIM!";
+    }
+}
