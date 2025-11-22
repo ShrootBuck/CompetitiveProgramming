16376bc99f5ee2dc18e63f97fd0ddb74b8716643
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-21 17:10:36 -0700

    Add Codeforces problem 1555 PizzaForces solution

---
+++ Codeforces/1555/pizzaforces.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+ll solve() {
+    ll n;
+    cin >> n;
+
+    return max(6LL, n + 1) / 2 * 5;
+}
+
+int main() {
+    setIO();
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
