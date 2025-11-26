2dcc2acd801b579e159b41a3732c0093009a7179
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-26 08:05:17 -0700

    Add Codeforces problem 104417 orders solution

---
+++ Codeforces/104417/orders.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+string solve() {
+    int n, k;
+    cin >> n >> k;
+
+    map<ll, ll> orders{};
+    for (int i = 0; i < n; i++) {
+        ll a, b;
+        cin >> a >> b;
+        orders[a] += b;
+    }
+
+    ll last_day = 0;
+    ll units = 0;
+    for (auto& order : orders) {
+        units += (order.first - last_day) * k;
+        units -= order.second;
+
+        if (units < 0)
+            return "No";
+
+        last_day = order.first;
+    }
+
+    return "Yes";
+}
+
+int main() {
+    setIO();
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        cout << solve() << "\n";
+    }
+}
