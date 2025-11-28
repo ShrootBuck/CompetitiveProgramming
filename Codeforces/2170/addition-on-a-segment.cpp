3e3570623e12d49386efc077419232bfda5f2b6a
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-28 12:41:21 -0700

    Add Codeforces 2170 solutions for problems A and B

---
+++ Codeforces/2170/addition-on-a-segment.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int solve() {
+    int n;
+    cin >> n;
+
+    vector<int> b(n);
+    ll S = 0, nonzeros = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        S += b[i];
+        nonzeros += b[i] != 0;
+    }
+
+    ll max_budget = S - (n - 1);
+    return min(max_budget, nonzeros);
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
---
+++ Codeforces/2170/maximum-neighborhood.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int solve() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> grid(n, vector<int>(n));
+
+    // this is inefficient as hell but ill upsolve later
+    int last = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            grid[i][j] = ++last;
+        }
+    }
+
+    int max_cost = 0;
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < n; c++) {
+            int local_cost = grid[r][c];
+
+            if (r > 0) {
+                local_cost += grid[r - 1][c];
+            }
+            if (r < n - 1) {
+                local_cost += grid[r + 1][c];
+            }
+            if (c > 0) {
+                local_cost += grid[r][c - 1];
+            }
+            if (c < n - 1) {
+                local_cost += grid[r][c + 1];
+            }
+
+            max_cost = max(max_cost, local_cost);
+        }
+    }
+
+    return max_cost;
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
