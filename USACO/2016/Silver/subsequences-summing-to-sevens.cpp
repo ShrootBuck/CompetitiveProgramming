de6ef1d85d2a651adb9e49bdba7a950f296b0562
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-30 13:08:13 -0700

    Add USACO 2016 Silver subsequences summing to sevens solution

---
+++ USACO/2016/Silver/subsequences-summing-to-sevens.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("div7");
+
+    int N;
+    cin >> N;
+
+    vector<int> cows(N);
+    for (int i = 0; i < N; i++) {
+        cin >> cows[i];
+    }
+
+    int range = 0;
+    vector<int> first(7, -1);
+    first[0] = 0;
+
+    int remainder = 0;
+    for (int i = 1; i <= N; i++) {
+        int cow;
+        cin >> cow;
+
+        remainder = (remainder + cow) % 7;
+        if (first[remainder] == -1) {
+            first[remainder] = i;
+        } else {
+            range = max(range, i - first[remainder]);
+        }
+    }
+    cout << range;
+}
