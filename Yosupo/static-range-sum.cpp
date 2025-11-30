822fdb6c62badb456b05f68a5da43ae13c967b60
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-11-30 11:19:22 -0700

    Add solution for static range sum problem

---
+++ Yosupo/static-range-sum.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<ll> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    vector<ll> prefix = a;
+    for (int i = 1; i < N; i++) {
+        prefix[i] += prefix[i - 1];
+    }
+
+    for (int i = 0; i < Q; i++) {
+        int l, r;
+        cin >> l >> r;
+
+        ll left_remove = 0;
+
+        if (l != 0) {
+            left_remove = prefix[l - 1];
+        }
+
+        cout << prefix[r - 1] - left_remove << "\n";
+    }
+}
