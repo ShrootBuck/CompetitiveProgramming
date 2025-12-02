4d72e5690300bd9484c26b7a8bde409643c2dae0
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-01 18:50:58 -0700

    Add USACO 2015 Bronze cow-hopscotch solution

---
+++ USACO/2015/Bronze/cow-hopscotch.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int R, C;
+vector<vector<char>> grid;
+
+int solve(int r, int c) {
+    if (r == R - 1 && c == C - 1) {
+        return 1;
+    }
+
+    int ans = 0;
+    for (int i = r + 1; i < R; i++) {
+        for (int j = c + 1; j < C; j++) {
+            if (grid[r][c] != grid[i][j]) {
+                ans += solve(i, j);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    setIO("hopscotch");
+
+    cin >> R >> C;
+
+    grid.resize(R);
+    for (int r = 0; r < R; r++) {
+        string s;
+        cin >> s;
+        for (int c = 0; c < C; c++) {
+            grid[r].push_back(s[c]);
+        }
+    }
+
+    cout << solve(0, 0);
+}
