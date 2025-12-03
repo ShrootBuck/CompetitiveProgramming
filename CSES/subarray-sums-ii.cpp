cd2a29faab1d72c376022e1f2e8701ab66864674
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-03 15:23:24 -0700

    Add solution for CSES Subarray Sums II problem

---
+++ CSES/subarray-sums-ii.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    ll prefix = 0;
+    ll ans = 0;
+
+    map<ll, int> freq;
+    freq[0] = 1;
+
+    for (int i = 0; i < n; i++) {
+        prefix += a[i];
+        ans += freq[prefix - x];
+
+        freq[prefix]++;
+    }
+
+    cout << ans;
+}
