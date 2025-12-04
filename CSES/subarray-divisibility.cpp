479650b0ea9d7fdee6519e615098bf00da365e07
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-03 17:57:41 -0700

    Add CSES subarray divisibility solution

---
+++ CSES/subarray-divisibility.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int n;
+    cin >> n;
+
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    auto prefix = a;
+    for (int i = 1; i < n; i++) {
+        prefix[i] += prefix[i - 1];
+    }
+
+    ll ans = 0;
+
+    map<ll, int> freq{{0, 1}};
+    for (int i = 0; i < n; i++) {
+        int remainder = ((prefix[i] % n) + n) % n;
+
+        prefix[i] = remainder;
+        ans += freq[remainder];
+        freq[remainder]++;
+    }
+
+    cout << ans;
+}
