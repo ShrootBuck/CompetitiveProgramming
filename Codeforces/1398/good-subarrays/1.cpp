12acd83ac1b96f16ffdd3e2df15d783ee3917528
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-06 12:32:46 -0700

    Add solutions for Codeforces 1398 good-subarrays
    Two approaches: O(n²) brute force and O(n) prefix sum with counting.

---
+++ Codeforces/1398/good-subarrays/1.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int solve() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        a[i] = s[i] - '0';
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += a[j];
+            int len = j - i + 1;
+            if (sum == len) {
+                ans++;
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
---
+++ Codeforces/1398/good-subarrays/2.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+ll solve() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        b[i] = s[i] - '0' - 1;
+    }
+
+    ll ans = 0;
+    map<int, int> counts{{0, 1}};
+
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += b[i];
+        ans += counts[sum];
+        counts[sum]++;
+    }
+
+    return ans;
+}
+
+int main() {
+    setIO();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << solve() << "\n";
+    }
+}
