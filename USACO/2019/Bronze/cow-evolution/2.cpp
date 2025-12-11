7f2be3934cab708ab6b1b2d06b2ce837ca044d74
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-10 17:33:17 -0700

    Add new solution 1 month later for cow evolution, no editorial!

---
+++ USACO/2019/Bronze/cow-evolution/2.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("evolution");
+
+    int N;
+    cin >> N;
+
+    set<string> features;
+    set<set<string>> populations;
+    for (int i = 0; i < N; i++) {
+        int K;
+        cin >> K;
+
+        set<string> population;
+        for (int j = 0; j < K; j++) {
+            string s;
+            cin >> s;
+            population.insert(s);
+            features.insert(s);
+        }
+
+        populations.insert(population);
+    }
+
+    for (const string& feature : features) {
+        for (auto& p1 : populations) {
+            for (auto& p2 : populations) {
+                if (p1.count(feature) == 0 || p2.count(feature) == 0) {
+                    break;
+                }
+
+                if (p1.size() == p2.size()) {
+                    cout << "no";
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "yes";
+}
