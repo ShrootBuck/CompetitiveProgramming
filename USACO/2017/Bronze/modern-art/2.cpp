aa3b647d6a5bf6a5b31682f3cb058c0e484ca1b2
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-12 19:14:00 -0700

    Add USACO 2017 Bronze Modern Art solution 2

---
+++ USACO/2017/Bronze/modern-art/2.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("art");
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> grid(N, vector<int>(N));
+    set<int> visible;
+    vector<int> min_r(10, N), max_r(10, 0), min_c(10, N), max_c(10, 0);
+    for (int r = 0; r < N; r++) {
+        string s;
+        cin >> s;
+        for (int c = 0; c < N; c++) {
+            int n = s[c] - '0';
+            grid[r][c] = n;
+            visible.insert(n);
+
+            min_r[n] = min(min_r[n], r);
+            min_c[n] = min(min_c[n], c);
+            max_r[n] = max(max_r[n], r);
+            max_c[n] = max(max_c[n], c);
+        }
+    }
+
+    visible.erase(0);
+    auto candidates = visible;
+
+    for (auto& color : visible) {
+        for (int r = min_r[color]; r <= max_r[color]; r++) {
+            for (int c = min_c[color]; c <= max_c[color]; c++) {
+                if (grid[r][c] != color) {
+                    candidates.erase(grid[r][c]);
+                }
+            }
+        }
+    }
+
+    cout << candidates.size();
+}
