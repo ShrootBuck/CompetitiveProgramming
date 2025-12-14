45c0a46c7f57620c99b6b64fae87822291949e4f
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-14 11:55:24 -0700

    Add USACO Palindrome Game solution

---
+++ USACO/2024/Bronze/palindrome-game.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ll long long
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO();
+
+    int T;
+    cin >> T;
+    while (T--) {
+        string S;
+        cin >> S;
+        cout << ((S.back() == '0') ? 'E' : 'B') << "\n";
+    }
+}
