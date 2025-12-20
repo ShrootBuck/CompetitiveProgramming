b259c5799c446b0063c98f30e554c505c1a9b370
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-20 14:42:07 -0700

    Enable LOCAL_RUN and debug build
    Replace the ll macro with a using alias and streamline IO init to
    cin.tie(0)->sync_with_stdio(0). Make setIO provide a LOCAL_RUN fallback
    that opens f.in/f.out when no filename is given, and call setIO() from
    main.

--- template.cpp
+++ template.cpp
@@ -3,15 +3,21 @@
 #include <string>
 using namespace std;
 
-#define ll long long
+using ll = long long;
 
 void setIO(string name = "") {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0)->sync_with_stdio(0);
+
     if (!name.empty()) {
         freopen((name + ".in").c_str(), "r", stdin);
         freopen((name + ".out").c_str(), "w", stdout);
     }
+#ifdef LOCAL_RUN
+    else {
+        freopen("f.in", "r", stdin);
+        freopen("f.out", "w", stdout);
+    }
+#endif
 }
 
-int main() { setIO("f"); }
+int main() { setIO(); }
