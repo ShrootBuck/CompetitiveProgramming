6ef1d8e17ebff46bea0b46493fe795fccbd76083
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-24 18:08:58 -0700

    WYR experience medieval torture or look at your old code

---
+++ USACO/2016/Bronze/mowing-the-field/2.cpp
@@ -0,0 +1,73 @@
+#include <climits>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+using namespace std;
+
+using ll = long long;
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0);
+
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+#ifdef LOCAL_RUN
+    else {
+        freopen("f.in", "r", stdin);
+        freopen("f.out", "w", stdout);
+    }
+#endif
+}
+
+int main() {
+    setIO("mowing");
+
+    ll N;
+    cin >> N;
+
+    ll x = 0, y = 0;
+    ll ans = LLONG_MAX;
+    bool overlapped = false;
+
+    map<pair<ll, ll>, ll> coordinate_to_time;
+    for (ll i = 0, t = 0; i < N; i++) {
+        char dir;
+        ll steps;
+        cin >> dir >> steps;
+
+        while (steps--) {
+            switch (dir) {
+            case 'N':
+                y++;
+                break;
+            case 'E':
+                x++;
+                break;
+            case 'S':
+                y--;
+                break;
+            case 'W':
+                x--;
+                break;
+            }
+
+            if (coordinate_to_time.count({x, y})) {
+                ans = min(ans, t - coordinate_to_time[{x, y}]);
+                overlapped = true;
+            }
+
+            coordinate_to_time[{x, y}] = t;
+            t++;
+        }
+    }
+
+    if (overlapped) {
+        cout << ans;
+    } else {
+        cout << -1;
+    }
+}
