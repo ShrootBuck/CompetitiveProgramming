083a1e14bd5d6e6a3a7e6c51e93d1d7d15543414
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-27 12:30:31 -0700

    Rename milk-pails.cpp and add alternative solution

---
+++ USACO/2016/Bronze/milk-pails/2.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+using ll = long long;
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0);
+
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+#ifdef LOCAL_RUN
+    else {
+        freopen("f.in", "r", stdin);
+        freopen("f.out", "w", stdout);
+    }
+#endif
+}
+
+int main() {
+    setIO("pails");
+
+    ll X, Y, M;
+    cin >> X >> Y >> M;
+
+    ll max_fill = LLONG_MIN;
+    for (ll x = 0; x <= M / X; x++) {
+        for (ll y = 0; y <= M / Y; y++) {
+            ll amount = x * X + y * Y;
+            if (amount <= M) {
+                max_fill = max(max_fill, amount);
+            }
+        }
+    }
+
+    cout << max_fill;
+}
