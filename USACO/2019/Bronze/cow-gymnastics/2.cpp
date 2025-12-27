b476489f35b2d140abeb06029ecc1049f76d3eb0
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-12-27 12:21:26 -0700

    Rename cow-gymnastics.cpp and add alternative solution

---
+++ USACO/2019/Bronze/cow-gymnastics/2.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0);
+
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+#ifdef LOCAL_RUN
+    else {
+        freopen("f.in", "r", stdin);
+        freopen("f.out", "w", stdout);
+    }
+#endif
+}
+
+int main() {
+    setIO("gymnastics");
+
+    ll K, N;
+    cin >> K >> N;
+
+    vector<vector<ll>> sessions(K, vector<ll>(N));
+    for (ll i = 0; i < K; i++) {
+        for (ll j = 0; j < N; j++) {
+            cin >> sessions[i][j];
+        }
+    }
+
+    ll consistent = 0;
+
+    for (ll i = 1; i <= N - 1; i++) {
+        for (ll j = i + 1; j <= N; j++) {
+            bool consistency = find(sessions[0].begin(), sessions[0].end(), i) <
+                               find(sessions[0].begin(), sessions[0].end(), j);
+            bool valid = true;
+            for (auto& session : sessions) {
+                bool local_consistency = find(session.begin(), session.end(), i) <
+                                         find(session.begin(), session.end(), j);
+                if (local_consistency != consistency) {
+                    valid = false;
+                }
+            }
+            consistent += valid;
+        }
+    }
+
+    cout << consistent;
+}
